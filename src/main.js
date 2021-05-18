630019ed56876c6373a172be007215b942c84545
Wei Lun <garyganweilun@gmail.com>
2021-05-19 03:24:46 +0800

    changes

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import genres from './util/genres.js';
 new Vue(
     {
         el: '#app',
+        method: {
+            checkFilter(category, title, checked) {
+                console.log(category, title, checked);
+            }
+        },
         components: {
             'movie-list': {
                 template: `<div id="movie-list">
@@ -30,13 +35,32 @@ new Vue(
                 template: `<div id="movie-filter">
 							<h2>Filter results</h2>
                             <div class="filter-group">
-                            <check-filter v-for="genre in genres" title="My title"></check-filter>
-                            </div>
+                                <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+                                </div>
                             </div>`,
+                methods: {
+                    checkFilter(category, title, checked) {
+                        this.$emit('check-filter', category,title,checked);
+                    }
+                },
                 components: {
                     'check-filter': {
+                        data() {
+                            return {
+                                checked: false
+                            }
+                        },
                         props: [ 'title'],
-                        template: `<div>Filter</div>`
+                        template: `<div v-bind:class="{'check-filter': true, active:checked}" v-on:click="checkFilter">
+                                        <span class="checkbox"></span>
+                                        <span class="check-filter-title">{{ title }}</span>
+                                   </div>`,
+                        methods: {
+                            checkFilter() {
+                                this.checked = !this.checked;
+                                this.$emit('check-filter', 'genre', this.title, this.checked);
+                            }
+                        }
                     }
 
                 }
