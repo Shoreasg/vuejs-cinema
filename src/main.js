68821b752159094e9cb93f932f2bdc3e766a4465
Wei Lun <garyganweilun@gmail.com>
2021-05-23 23:51:08 +0800

    changes

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
 import './style.scss';
-import MovieList from './components/MovieList.vue'
-import MovieFilter from './components/MovieFilter.vue'
+import Overview from './components/Overview.vue';
 
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
-import moment from 'moment-timezone'
-Object.defineProperty(Vue.prototype, '$moment', {get() { return this.$root.moment } });
+import moment from 'moment-timezone';
+moment.tz.setDefault("UTC");
+Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 
+import { checkFilter } from './util/bus';
+const bus = new Vue();
+Object.defineProperty(Vue.prototype, '$bus', { get() { return this.$root.bus } });
 new Vue(
     {
         el: '#app',
@@ -16,29 +19,20 @@ new Vue(
             genre: [],
             time: [],
             movies: [],
-            moment
+            moment,
+            day: moment(),
+            bus
         },
             
-        methods: {
-            checkFilter(category, title, checked) {
-                if (checked) {
-                    this[category].push(title);
-                } else {
-                    let index = this[category].indexOf(title);
-                    if (index > -1) {
-                        this[category].splice(index, 1);
-                    }
-                }
-            }
-        },
+        
         components: {
-            MovieList,    
-            MovieFilter,
+          Overview
         },
         created() {
             this.$http.get('/api').then(response => {
                 this.movies = response.data;
             });
+            this.$bus.$on('check-filter', checkFilter.bind(this));
         }
 
     });
